149d2f69ff0079418a435b18fd86df5f64982fbe
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-06-03 23:20:10 +0300

    app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize/modern-normalize.css';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
